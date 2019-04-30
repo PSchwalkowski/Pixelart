3b60783e2eeb4ee61ad0188d245b2ee4feca2f9e
Paweł Schwałkowski <p.schwalkowski@gmail.com>
2019-04-30 16:52:28 +0200

    More settings

--- src/Pixelart.php
+++ src/Pixelart.php
@@ -16,6 +16,27 @@ class Pixelart {
 	 */
 	protected $squareSize;
 
+	/**
+	 * @var null
+	 */
+	protected $image = null;
+
+	/**
+	 * @var array
+	 */
+	protected $imageSize = [
+		'width' => 0,
+		'height' => 0,
+	];
+
+	/**
+	 * @var array
+	 */
+	protected $cropPartsSizes = [
+		'horizontal' => 0,
+		'vertical' => 0,
+	];
+
 
 	/**
 	 * Pixelart constructor.
@@ -24,7 +45,7 @@ class Pixelart {
 	 *
 	 * @throws Exception
 	 */
-	public function __construct(int $squareSize) {
+	public function __construct(int $squareSize = 10) {
 		$this->setSquareSize($squareSize);
 	}
 
@@ -45,21 +66,25 @@ class Pixelart {
 	}
 
 	/**
-	 * @param string $imagePath
+	 * @param string $filepath
 	 *
 	 * @return string
 	 * @throws Exception
 	 */
-	public function pixelize(string $imagePath): string {
-		if (!$this->checkIfFileExists($imagePath)) {
-			throw new Exception('File "' . $imagePath . '" could not be found.');
+	public function pixelize(string $filepath): string {
+		if (!$this->checkIfFileExists($filepath)) {
+			throw new Exception('File "' . $filepath . '" could not be found.');
 		}
-		if (!$this->checkIfFileExtensionIsSupported($imagePath)) {
+		if (!$this->checkIfFileExtensionIsSupported($filepath)) {
 			throw new Exception('Given file extension is not supported');
 		}
 
+		$this->setImageResourceFromPath($filepath)->setImageResourceSize($filepath)->setCropPartSizes();
 
-
+		echo '<pre>';
+		print_r($this);
+		echo '</pre>';
+		
 		return '';
 	}
 
@@ -75,9 +100,62 @@ class Pixelart {
 	/**
 	 * @param string $filepath
 	 *
+	 * @return mixed
+	 */
+	protected function getFileExtension(string $filepath) {
+		return pathinfo($filepath, PATHINFO_EXTENSION);
+	}
+
+	/**
+	 * @param string $filepath
+	 *
 	 * @return bool
 	 */
 	protected function checkIfFileExtensionIsSupported(string $filepath): bool {
-		return in_array(pathinfo($filepath, PATHINFO_EXTENSION), self::SUPPORTED_EXTENSIONS);
+		return in_array($this->getFileExtension($filepath), self::SUPPORTED_EXTENSIONS);
+	}
+
+	/**
+	 * @param string $filepath
+	 *
+	 * @return Pixelart
+	 */
+	protected function setImageResourceFromPath(string $filepath): self {
+		switch ($this->getFileExtension($filepath)) {
+			case 'jpg':
+			case 'jpeg':
+				$this->image = imagecreatefromjpeg($filepath);
+				break;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * @param string $filepath
+	 *
+	 * @return Pixelart
+	 */
+	protected function setImageResourceSize(string $filepath): self {
+		$size = getimagesize($filepath);
+
+		$this->imageSize = [
+			'width' => $size[0],
+			'height' => $size[1],
+		];
+
+		return $this;
+	}
+
+	/**
+	 * @return $this
+	 */
+	private function setCropPartSizes() {
+		$this->cropPartsSizes = [
+			'horizontal' => $this->imageSize['width'] / $this->squareSize,
+			'vertical' => $this->imageSize['height'] / $this->squareSize,
+		];
+
+		return $this;
 	}
 }
